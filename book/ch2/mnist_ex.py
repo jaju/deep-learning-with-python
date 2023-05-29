1c10774f98c33c22cebaae7c9438132dc83ba277
Ravindra Jaju <jaju@msync.org>
2023-05-29 18:36:39 +0530

    More code from chapter 2, and a notebook

---
+++ book/ch2/mnist_ex.py
@@ -0,0 +1,26 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.datasets import mnist
+
+
+def init_model():
+    model = keras.Sequential([
+        layers.Dense(512, activation="relu"),
+        layers.Dense(10, activation="softmax")
+    ])
+    model.compile(optimizer="rmsprop", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+    return model
+
+
+def load_mnist():
+    return mnist.load_data()
+
+
+def reshape_mnist(train_data, test_data):
+    (train_images, train_labels) = train_data
+    (test_images, test_labels) = test_data
+    train_images = train_images.reshape((60000, 28 * 28))
+    train_images = train_images.astype("float32") / 255
+    test_images = test_images.reshape((10000, 28 * 28))
+    test_images = test_images.astype("float32") / 255
+    return (train_images, train_labels), (test_images, test_labels)
---
+++ book/ch2/tensors.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import book.ch2.mnist_ex as mnist_ex
+
+def header(msg):
+    print("\n" + msg)
+    print("-" * len(msg))
+
+
+# Rank 0 tensors
+header("Rank 0")
+x = np.array(12)
+print(x)
+print(f"Dimensions {x.ndim}")
+
+# Rank 1 tensors
+header("Rank 1")
+x = np.array([2, 3, 6, 9, 15])
+print(x)
+print(f"Dimensions = {x.ndim}")
+
+# Rank 2 tensors
+header("Rank 2")
+x = np.array([[1, 2, 3, 4, 5],
+              [10, 20, 30, 40, 50],
+              [100, 200, 300, 400, 500]])
+print(x)
+print(f"Dimensions = {x.ndim}")
+
+(train_images, train_labels), (test_images, test_labels) = mnist_ex.load_mnist()
+
+digit = train_images[4]
+plt.imshow(digit, cmap=plt.cm.binary)
+plt.show()
