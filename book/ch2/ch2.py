cbb973f3704d93f3f4996768bc107338ae42cac5
Ravindra Jaju <jaju@msync.org>
2023-05-29 14:22:27 +0530

    Initial commit

---
+++ book/ch2/ch2.py
@@ -0,0 +1,3 @@
+from tensorflow.keras.datasets import mnist
+
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
---
+++ book/soundcheck/packages.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+
+example_shape = (3, 3)
+print(tf.zeros(example_shape))
+
+another_example_shape = (4, 2)
+print(tf.ones(another_example_shape))
